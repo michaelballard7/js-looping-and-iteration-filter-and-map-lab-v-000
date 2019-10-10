46e8bd9ed12794ee4151b979d1b58f25725a6670
Michael Ballard <ballard.michael9@gmail.com>
2019-10-10 00:00:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,13 @@
 function driversWithRevenueOver(drivers, number){
     return drivers.filter((driver)=> driver.revenue > number)
 }
+
+function driverNamesWithRevenueOver(drivers, number){
+    let driversNames = [];
+    drivers.filter((driver) => {
+        if (driver.revenue > number){
+            driversNames.push(driver.name)
+        }
+    })
+    return driversNames;
+}
